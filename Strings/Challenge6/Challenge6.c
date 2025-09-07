a4f6b14572f3acb334adec6cf7a6ee5cbf87ff3d
Ayb001 <aghjedimayoub@gmail.com>
2025-09-07 20:03:44 +0100

    test

---
+++ Strings/Challenge6/Challenge6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    char str1[100];
+    int i=0;
+    char c;
+    int count=0;
+    printf("entrer la chaine:");
+    scanf("%s",str1);
+    printf("entrer le Caractère chercher:");
+    scanf("%c",&c);
+    while(str1[i]!='\0')
+    {
+        if(str[i]==c)
+        {
+            count++:
+        }
+        i++;
+    }
+}
